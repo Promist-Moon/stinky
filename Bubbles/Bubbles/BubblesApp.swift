51509825aa1895f16a0e7c59e7fc59d60ed12ae3
binghangc <binghangc@gmail.com>
2025-01-18 12:33:58 +0800

    Hi everyone

---
+++ Bubbles/Bubbles/BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by Bing Hang on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bubbles/Bubbles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bubbles
+//
+//  Created by Bing Hang on 18/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
